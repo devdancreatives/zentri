adb71f472371877b9b93147da024d9c8995e4504
danny235 <danielbarima235@gmail.com>
2026-01-09 15:21:25 +0100

    fix: refactor graphql route to resolve 405 error on vercel

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,9 +1,10 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import { typeDefs } from "@/graphql/schema";
 import { resolvers } from "@/graphql/resolvers";
-import { NextRequest, NextResponse } from "next/server";
 
-const { handleRequest } = createYoga({
+export const dynamic = "force-dynamic";
+
+const yoga = createYoga({
   schema: createSchema({
     typeDefs,
     resolvers,
@@ -12,11 +13,4 @@ const { handleRequest } = createYoga({
   fetchAPI: { Response },
 });
 
-// Wrappers for Next.js App Router
-export async function GET(request: NextRequest, context: any) {
-  return handleRequest(request, context);
-}
-
-export async function POST(request: NextRequest, context: any) {
-  return handleRequest(request, context);
-}
+export { yoga as GET, yoga as POST, yoga as OPTIONS };
