74d2c197bc2d0d0638ed10b1c8548970e824e5e9
danny235 <danielbarima235@gmail.com>
2026-01-20 08:22:53 +0100

    fix: 405 errors

--- next.config.ts
+++ next.config.ts
@@ -13,26 +13,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 });
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/api/graphql",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
-          },
-        ],
-      },
-    ];
-  },
   experimental: {
     serverComponentsExternalPackages: ["tiny-secp256k1"],
   },
